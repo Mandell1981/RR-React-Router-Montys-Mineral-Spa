345150625d99d13357970c912b449f7cd61ced70
Mandell1981 <Mandell1981.rm@gmail.com>
2024-02-22 21:00:34 -0500

    Finished

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,8 +1,8 @@
-
-import './App.css';
-
+i
 import Home from './components/Home'
-import About from './components/About'
+import {BrowserRouter as Router, Link, Routes, Route} from 'react-router-dom'
+import './App.css';
+mport About from './components/About'
 import Packages from './components/Packages'
 
 
@@ -12,24 +12,33 @@ function App() {
 
   return (
     <div className="App">
+      <Router>
       <header>
         <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
         <div className="navBar">
           <ul>
             <li>
-              <a href={<Home />}>Home</a>
+              <Link to={<Home />}>Home</Link>
             </li>
             <li>
-              <a href={<About />}>About Us</a>
+              <Link to={<About />}>About Us</Link>
             </li>
             <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
+              <Link to={<Packages packages={packages}/>}>Our Packages</Link>
             </li>
           </ul>
         </div>
 
       </header>
+      <div className="display">
+        <Routes>
+          <Route path="/"element={<Home />} />
+          <Route path="/about"element={<About />} />
+          <Route path="/packages"element={<Packages packages={packages}/>} />
+        </Routes>
+      </div>
+      </Router>
     </div>
   );
 }
